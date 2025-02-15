62923f5f5b42de32e553c495349a90a78344c903
Mrbrookjmedia <mr.brookjmedia@gmail.com>
2025-02-15 11:33:04 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+app.set("trust proxy", 1);
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
