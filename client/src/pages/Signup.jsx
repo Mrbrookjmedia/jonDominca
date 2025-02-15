a196b70c2cc04443efca551ba1c3001e9b666367
Suraj Bhatt <samnoty235@gmail.com>
2025-02-15 10:21:28 +0530

    comment out Signup.css and added .nvmrc file with older node

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/SignUp.css";
+// import "../styles/SignUp.css";
 import apiRequest from "../lib/apiRequest";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
